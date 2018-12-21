9241f9488604d757cb949b0751560894f83820e2
roshith93 <r.roshith@gmail.com>
2018-12-21 19:33:49 +0530

    counter using switch cases

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    counter : 0
+  }
+  handleChange = (action, value) => {
+    switch(action){
+      case 'add':
+        this.setState(prevState => { return{ counter : prevState.counter + 1} })
+        break;
+      case 'sub':
+        this.setState(prevState => { return {counter:prevState.counter -1}})
+        break;
+      case 'inc5':
+      this.setState(prevState => {return {counter : prevState.counter + value}})
+      break;
+      case 'dec5':
+      this.setState(prevState => { return {counter : prevState.counter - value}})
+      break;
+      default : 
+        return this.state.counter;
+    }
+   
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+       <h1>Counter</h1>
+       <button onClick={() => this.handleChange('add')}>+ 1</button>
+       <button onClick={() => this.handleChange('sub')}> -1 </button>
+
+       <h3>{this.state.counter}</h3>
+       <button onClick={() => this.handleChange('inc5',5)}> + 5</button>
+       <button onClick={() => this.handleChange('dec5',5)}> -5</button>
       </div>
     );
   }
